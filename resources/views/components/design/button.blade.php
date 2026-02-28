663997290dc74f26c9a6cf9570004679d3b91dc4
= <=>
2026-02-28 14:44:41 -0600

    make tabs gray instead of black

--- resources/views/components/design/button.blade.php
+++ resources/views/components/design/button.blade.php
@@ -1,15 +1,12 @@
 <button
-    
-    class="font-medium px-3 py-2 transition duration-150 ease-in-out flex items-center justify-center"
-
-    :class="category === '{{ $category }}' ? 'bg-black' : 'bg-white hover:bg-gray-200'"
-    
-    @click="category = '{{ $category }}'"
+  class="font-medium px-3 py-2 transition duration-150 ease-in-out flex items-center justify-center"
+  :class="{
+    'bg-gray-700': category === '{{ $category }}',
+    'bg-white hover:bg-gray-200': category !== '{{ $category }}'
+  }"
+  @click="category = '{{ $category }}'"
 >
-    <span 
-    
-    :class="category === '{{ $category }}' ? 'text-white font-bold' : 'text-gray-600'">{{ $slot }}
-
-    </span>
-    
+  <span :class="category === '{{ $category }}' ? 'text-white font-bold' : 'text-gray-600'">
+    {{ $slot }}
+  </span>
 </button>
