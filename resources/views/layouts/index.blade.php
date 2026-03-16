ec3b6a42c4df20da09c2b81a2fae6ebb020a3033
= <=>
2026-03-15 21:13:33 -0500

    fix layout index hero

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -35,7 +35,7 @@
             <!-- RIGHT COLUMN -->
             <div class="hidden md:flex justify-end">
                 <img 
-                    src="{{ asset('images/covers-index.webp') }}" 
+                    src="{{ $featuredLayout->hero_url }}" 
                     alt="Featured Covers"
                     class="w-full rotate-1"
                 >
